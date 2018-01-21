1a912da30016233e42ae7d75491054a558ac3b92
Melissa Mepham <m.a.mepham@gmail.com>
2018-01-21 18:17:18 +1300

    setting up the file

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var rot = 60
+var rote = 15
+var rotp = 30
+var s = 1
+
+function setup() {
+    createCanvas(900, 900);
+}
+
+function draw(){
+    background(255, 242, 235);
+    noStroke()
+    fill(255, 116, 203);
+
+}
