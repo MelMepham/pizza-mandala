e68023624b3b81045bc7f8dab43c11c2fdd81128
Melissa Mepham <m.a.mepham@gmail.com>
2018-01-22 18:00:57 +1300

    update files

---
+++ shapes.js
@@ -0,0 +1,23 @@
+module.exports = centralCircle
+
+
+
+
+function centralCircle() {
+  push()
+  translate(450, 450);
+  fill(255, 98, 255);
+  triangle(-52, 30, -52, -30, 0, 0);
+  rotate(radians(sixtyDegrees))
+  triangle(-52, 30, -52, -30, 0, 0);
+  rotate(radians(sixtyDegrees))
+  triangle(-52, 30, -52, -30, 0, 0);
+  rotate(radians(sixtyDegrees))
+  triangle(-52, 30, -52, -30, 0, 0);
+  rotate(radians(sixtyDegrees))
+  triangle(-52, 30, -52, -30, 0, 0);
+  triangle(-52, 30, -52, -30, 0, 0);
+  rotate(radians(sixtyDegrees))
+  triangle(-52, 30, -52, -30, 0, 0);
+  pop()
+}
--- sketch.js
+++ sketch.js
@@ -1,16 +1,77 @@
+// var shapes = require('./shapes')
+// console.log(shapes)
 
-var rot = 60
-var rote = 15
-var rotp = 30
-var s = 1
+function rotation(degrees) {
+  rotate(radians(degrees))
+}
+
 
 function setup() {
     createCanvas(900, 900);
 }
 
 function draw(){
-    background(255, 242, 235);
-    noStroke()
-    fill(255, 116, 203);
 
-}
+    background(255, 242, 235);
+    noStroke();
+
+    push()
+    fill(247, 76, 207);
+    tint(255, 127);
+      ellipse(450, 450, 120, 120);
+      fill(247, 175, 207);
+      ellipse(390, 450, 120, 120);
+    pop()
+
+    push()
+    translate(450, 450);
+    fill(0, 242, 199);
+      triangle(-52, 30, -52, -30, 0, 0);
+      rotation(60)
+      triangle(-52, 30, -52, -30, 0, 0);
+      rotation(60)
+      triangle(-52, 30, -52, -30, 0, 0);
+      rotation(60)
+      triangle(-52, 30, -52, -30, 0, 0);
+      rotation(60)
+      triangle(-52, 30, -52, -30, 0, 0);
+      rotation(60)
+      triangle(-52, 30, -52, -30, 0, 0);
+    pop()
+
+    push()
+    translate(450, 450);
+    fill(197, 255, 215);
+      triangle(-52, -30, 0, 0, -35, 0)
+      rotation(60)
+      triangle(-52, -30, 0, 0, -35, 0)
+      rotation(60)
+      triangle(-52, -30, 0, 0, -35, 0)
+      rotation(60)
+      triangle(-52, -30, 0, 0, -35, 0)
+      rotation(60)
+      triangle(-52, -30, 0, 0, -35, 0)
+      rotation(60)
+      triangle(-52, -30, 0, 0, -35, 0)
+    pop()
+
+    push()
+    translate(450, 450);
+    fill(0, 144, 207);
+      triangle(-52, 30, -35, 0, 0, 0)
+      rotation(60)
+      triangle(-52, 30, -35, 0, 0, 0)
+      rotation(60)
+      triangle(-52, 30, -35, 0, 0, 0)
+      rotation(60)
+      triangle(-52, 30, -35, 0, 0, 0)
+      rotation(60)
+      triangle(-52, 30, -35, 0, 0, 0)
+      rotation(60)
+      triangle(-52, 30, -35, 0, 0, 0)
+    pop()
+
+
+
+
+  }
