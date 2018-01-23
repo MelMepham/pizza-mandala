e3660f13964b0ecf84679fc09005ccb13386645c
Melissa Mepham <m.a.mepham@gmail.com>
2018-01-24 08:37:13 +1300

    entered the flower of life

--- sketch.js
+++ sketch.js
@@ -15,15 +15,32 @@ function draw(){
     background(255, 242, 235);
     noStroke();
 
-    push()
-    fill(247, 76, 207);
-    tint(255, 127);
-      ellipse(450, 450, 120, 120);
+    push() //FLOWER OF LIFE PATTERN
+      ellipse(450,450,210,210)
+    translate(450, 450);
+    blendMode(MULTIPLY);
       fill(247, 175, 207);
-      ellipse(390, 450, 120, 120);
+      ellipse(-60, 0, 120, 120);
+      rotation(60)
+      ellipse(-60, 0, 120, 120);
+      rotation(60)
+      ellipse(-60, 0, 120, 120);
+      rotation(60)
+      ellipse(-60, 0, 120, 120);
+      rotation(60)
+      ellipse(-60, 0, 120, 120);
+      rotation(60)
+      ellipse(-60, 0, 120, 120);
     pop()
 
-    push()
+    push() //CIRCLE FRAMING THE HEXAGON IN THE CENTER
+    blendMode(MULTIPLY);
+    translate(450, 450);
+    fill(247, 76, 207);
+    ellipse(0, 0, 120, 120);
+    pop()
+
+    push() //DARKEST TRIANGLE IN THE CENTRAL HEXAGON SHAPE
     translate(450, 450);
     fill(0, 242, 199);
       triangle(-52, 30, -52, -30, 0, 0);
@@ -39,7 +56,7 @@ function draw(){
       triangle(-52, 30, -52, -30, 0, 0);
     pop()
 
-    push()
+    push() //LIGHTEST TRIANGLE IN THE CENTRAL HEXAGON SHAPE
     translate(450, 450);
     fill(197, 255, 215);
       triangle(-52, -30, 0, 0, -35, 0)
@@ -55,7 +72,7 @@ function draw(){
       triangle(-52, -30, 0, 0, -35, 0)
     pop()
 
-    push()
+    push() // CENTRAL TURQUOIUSE HEXAGON
     translate(450, 450);
     fill(0, 144, 207);
       triangle(-52, 30, -35, 0, 0, 0)
